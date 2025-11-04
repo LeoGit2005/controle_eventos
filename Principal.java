571e48f6d17e1bea5732095d7ab9283ab69ba1eb
Leonardo Freitas de Oliveira <leonardofreitas.2005@gmail.com>
2025-11-04 12:12:05 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,11 @@
+package com.exemplo.controleeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEventosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ControleEventosApplication.class, args);
+    }
+}
